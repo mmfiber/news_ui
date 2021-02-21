2b66942e7972676a74eb90b77ae506a2ae557eb6
mmfiber <masa.03191996@gmail.com>
2021-02-21 21:48:58 +0900

    without sudo

--- setup.sh
+++ setup.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 ssh -t -v ${USER_NAME}@${HOST_NAME} << EOS
 '
-sudo cd /home/${USER_NAME}/news_ui
-sudo ls -a
-sudo git pull
-sudo npm i
-sudo npm run build
-sudo npm start
+cd ~/news_ui
+git pull
+npm i
+npm run build
+npm start
 '
 EOS
