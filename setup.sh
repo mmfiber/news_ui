283ccd92e0944d0d2ad937901f7e976f9bf04f2c
mmfiber <masa.03191996@gmail.com>
2021-02-21 21:42:37 +0900

    with quotre

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 ssh -t -v ${USER_NAME}@${HOST_NAME} << EOS
-sudo cd ~/news_ui \
-  && ls -a \
-  && git pull \
-  && npm i \
-  && npm run build \
-  && npm start
+'
+sudo cd ~/news_ui
+sudo ls -a
+sudo git pull
+sudo npm i
+sudo npm run build
+sudo npm start
+'
 EOS
