5305b3a4c3e3285c7d3df92399393f48bace76a8
mmfiber <masa.03191996@gmail.com>
2021-02-21 22:05:07 +0900

    test

--- setup.sh
+++ setup.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-ssh -t -v ${USER_NAME}@${HOST_NAME} << EOS
-'
-cd /home/ec2-user/news_ui
+ssh -t -v ${USER_NAME}@${HOST_NAME} << 'EOS'
+cd ~/news_ui
 git pull
 npm i
 npm run build
 npm start
-'
 EOS
